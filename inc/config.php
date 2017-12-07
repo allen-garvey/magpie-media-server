f08ed725c530d47e5ed042b2dc1b66052efe0354
User <zipper871@hotmail.com>
2017-12-07 02:18:12 +0000

    Edit book and video constants to use shared media constant

--- inc/config.php
+++ inc/config.php
@@ -5,6 +5,7 @@ define('INC_PATH', ROOT_PATH.'inc/');
 define('VIEWS_PATH', INC_PATH.'views/');
 define('CONTROLLERS_PATH', INC_PATH.'controllers/');
 define('STYLES_PATH', ROOT_PATH.'public_html/styles/');
+define('MEDIA_PATH', ROOT_PATH.'public_html/media/');
 
 
 require_once(INC_PATH.'environment_setup.php');
@@ -13,19 +14,20 @@ require_once(INC_PATH.'environment_setup.php');
 define('BASE_URL', '/');
 define('STYLES_URL', BASE_URL.'styles/');
 define('SCRIPTS_URL', BASE_URL.'scripts/');
+define('MEDIA_URL', BASE_URL.'media/');
 
 
 //videos
 define('VIDEOS_DIR_NAME', 'videos');
-define('VIDEOS_PATH', ROOT_PATH.'public_html/media/'.VIDEOS_DIR_NAME.'/');
+define('VIDEOS_PATH', MEDIA_PATH.VIDEOS_DIR_NAME.'/');
 define('VIDEOS_INDEX_URL', BASE_URL.VIDEOS_DIR_NAME.'/');
-define('VIDEOS_URL', BASE_URL.'media/'.VIDEOS_DIR_NAME.'/');
+define('VIDEOS_URL', MEDIA_URL.VIDEOS_DIR_NAME.'/');
 
 //books
 define('BOOKS_DIR_NAME', 'books');
-define('BOOKS_PATH', ROOT_PATH.'public_html/media/'.BOOKS_DIR_NAME.'/');
+define('BOOKS_PATH', MEDIA_PATH.BOOKS_DIR_NAME.'/');
 define('BOOKS_INDEX_URL', BASE_URL.BOOKS_DIR_NAME.'/');
-define('BOOKS_URL', BASE_URL.'media/'.BOOKS_DIR_NAME.'/');
+define('BOOKS_URL', MEDIA_URL.BOOKS_DIR_NAME.'/');
 
 
 define('SITE_TITLE', 'Magpie Media Server');
